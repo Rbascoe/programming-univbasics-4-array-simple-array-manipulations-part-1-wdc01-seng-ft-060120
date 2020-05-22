f82d60305adb82adf3be086325d7e2752c6f02da
Roan Bascoe <roanbascoe5@gmail.com>
2020-05-22 19:09:31 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push (array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  colors_in_the_rainbow << "violet"
+end
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  bouroughs_in_nyc.unshift "Staten Island"
+end
+
+def using_pop(array)
+  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  @continents.pop 
+end
+
+def pops_with_args
+
+end
+
+def using shift
+
+end
+
+def shift_with_args
+
+end
